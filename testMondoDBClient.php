98769380ca46106a6c30403ce8840c369aa13efb
santobuonoivan <mecaluxivansantobuono@gmail.com>
2019-04-24 11:25:35 -0300

    upgrade dependencies

---
+++ testMondoDBClient.php
@@ -0,0 +1,14 @@
+<?php
+
+    declare(strict_types=1);
+    
+    require "./vendor/autoload.php";
+    require "./BaseDeDatos.php";
+    
+
+    use PHPUnit\Framework\TestCase;
+
+    final class EmailTest extends TestCase
+    {
+        
+    }
